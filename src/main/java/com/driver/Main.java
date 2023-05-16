23d7c277a6685c116c8524e45cda5e12eaaab975
shad <princekhan785@gmail.com>
2023-05-17 03:58:54 +0530

    overriden the method meth in class b

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,10 @@ class A{
     }
 }
 class B extends A{
-
+    @Override
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+    }
 }
 public class Main {
     public static void main(String[] args){
