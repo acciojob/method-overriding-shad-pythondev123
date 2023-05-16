fa9281e9618bc89c444f9742c60b20a6697ef04c
shad <princekhan785@gmail.com>
2023-05-17 03:57:15 +0530

    resolved errors , defined class a and b in main file. created a method for class a and implemented with obj of class b

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
 
+}
 public class Main {
     public static void main(String[] args){
         B ans = new B();
