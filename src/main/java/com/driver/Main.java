0710863e8d3c3bc5937ab946288020466d3dd100
shad <princekhan785@gmail.com>
2023-06-20 16:22:09 +0530

    called meth

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,10 @@
 package com.driver;
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
 
-    public String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
 public class Main {
     public static void main(String[] args){
-        B ans = new B();
-    ans.meth();
 
+     B b= new B();
+     System.out.println(b.meth());
     }
 
 }
